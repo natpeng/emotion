29bc0ab0c9a9a4269b6e21bea8d7a278fcf0e75a
Natalie(Jiayuan) Peng <jiayuan.peng@gmail.com>
2018-08-24 15:54:21 -0400

    master update

--- tables.py
+++ tables.py
@@ -1,14 +1,19 @@
 from database import engine
+from sqlalchemy import Table, Column, Integer, String, MetaDta, ForeignKey
+
+metadata = MetaData(engine)
 
 User.__table__
-Table('user', MetaData(engine),
+Table('user', metadata,
     Column('uid', String(32), primary_key=True, nullable=False),
     Column('name', String(32)), schema=None)
 
 Emotion.__table__
-Table('emotion', MetaData(engine),
+Table('emotion', metadata,
     Column('id', Integer(), primary_key=True, nullable=False),
-    Column('uid', String(32)),
+    Column('uid', String(32), ForeignKey('user.uid')),
     Column('date', String(10)),
     Column('timestamp', String(32)),
     Column('rating', Integer()), schema=None)
+
+metadata.create_all()
